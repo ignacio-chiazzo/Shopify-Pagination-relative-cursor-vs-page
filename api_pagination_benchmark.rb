74886918a52733e0ac9a22d8f923fbb91056bb3a
Ignacio Chiazzo <ignaciochiazzo@gmail.com>
2020-03-15 11:40:27 -0400

    move to classes

--- api_pagination_benchmark.rb
+++ api_pagination_benchmark.rb
@@ -20,83 +20,97 @@ module Constants
   LIMIT_PER_PAGE = 50
 end
 
-class BenchmarkShopifyAPIPagination
-  extend Constants
-  MODEL = ShopifyAPI::Product # You can change this value to any other class that has an index and a count action such as `ShopifyAPI::Customer, ShopifyAPI::Order, ShopifyAPI::Collect, ShopifyAPI::SmartCollection, ShopifyAPI::CustomCollection`
-
-  class << self
-    def test_using_page
-      api_version = API_VERSION_USING_PAGE
-      authenticate(api_version: api_version)
-
-      page = 1
-      benchmark_time(final_message_time(api_version)) do
-        records = query_records_using_page(page)
-        while(records.count == LIMIT_PER_PAGE)
-          page += 1
-          records = query_records_using_page(page)
-        end
-      end
-    end
-
-    def test_using_cursor_based_paginator
-      api_version = API_VERSION_USING_CURSOR_RELATIVE
-      authenticate(api_version: api_version)
-
-      page = 1
-      benchmark_time(final_message_time(api_version)) do
-        records = benchmark_time(message_for_query(api_version, page)) do
-          MODEL.find(:all, params: { limit: LIMIT_PER_PAGE })
-        end
-        while records.next_page?
-          page += 1
-          records = benchmark_time(message_for_query(api_version, page)) do
-            records.fetch_next_page
-          end
-        end
-      end
-    end
-
-    private
-
-    def message_for_query(api_version, page)
-      if api_version == API_VERSION_USING_CURSOR_RELATIVE
-        "Time to get page #{page} for #{MODEL.name.demodulize}s USING CURSOR BASED PAGINATION: "
-      elsif api_version == API_VERSION_USING_PAGE
-        "Time to get page #{page} for #{MODEL.name.demodulize}s  USING PAGE : "
-      end
-    end
-
-    def final_message_time(api_version)
-      if api_version == API_VERSION_USING_CURSOR_RELATIVE
-        "\e[32m Time to iterate over all #{MODEL.name.demodulize}s using CURSOR BASED PAGINATION: \e[0m"
-      elsif api_version == API_VERSION_USING_PAGE
-        "\e[31m Time to iterate over all #{MODEL.name.demodulize}s using PAGE: \e[0m"
-      end
-    end
-
-    def query_records_using_page(page)
-      benchmark_time(message_for_query(API_VERSION_USING_PAGE, page)) do
-        MODEL.find(:all, params: { limit: LIMIT_PER_PAGE, page: page })
-      end
-    end
-
-    def authenticate(api_version:)
-      shopify_session = ShopifyAPI::Session.new(domain: DOMAIN, token: ACCESS_TOKEN, api_version: api_version, extra: nil)
-      ShopifyAPI::Base.activate_session(shopify_session)
-    end
-
-    def benchmark_time(message, &block)
-      time = Time.now.utc
-      begin
-        yield
-      ensure
-        duration_ms = (Time.now.utc - time) * 1000
-        puts "#{message} #{duration_ms.round(2)} ms \n"
-      end
+module BenchmarkPrinter
+  def benchmark_time(message, &block)
+    time = Time.now.utc
+    begin
+      yield
+    ensure
+      duration_ms = (Time.now.utc - time) * 1000
+      puts "#{message} #{duration_ms.round(2)} ms \n"
     end
   end
 end
 
-BenchmarkShopifyAPIPagination.test_using_page
-BenchmarkShopifyAPIPagination.test_using_cursor_based_paginator
+class PageBasedPaginate
+  extend Constants
+  extend BenchmarkPrinter
+
+  def initialize
+    @api_version = Constants::API_VERSION_USING_PAGE
+    @shopify_session = ShopifyAPI::Session.new(domain: Constants::DOMAIN, token: Constants::ACCESS_TOKEN, api_version: @api_version, extra: nil)
+    @limit = Constants::LIMIT_PER_PAGE
+  end
+
+  def test_using_page(model)
+    ShopifyAPI::Base.activate_session(@shopify_session)
+    page = 1
+    self.class.benchmark_time(final_message_time(model)) do # TODO: check class
+      records = query_records_using_page(model, page)
+      while(records.count == @limit)
+        page += 1
+        records = query_records_using_page(model, page)
+      end
+    end
+  end
+
+  private
+
+  def query_records_using_page(model, page)
+    self.class.benchmark_time(message(model, page)) do
+      model.find(:all, params: { limit: @limit, page: page })
+    end
+  end
+
+  def message(model, page)
+    "Time to get page #{page} for #{model}s  USING PAGE : "
+  end
+
+  def final_message_time(model)
+    "\e[31m Time to iterate over all #{model}s using PAGE: \e[0m"
+  end
+end
+
+class CurosrBasedPaginate
+  extend Constants
+  extend BenchmarkPrinter
+
+  def initialize
+    @api_version = Constants::API_VERSION_USING_CURSOR_RELATIVE
+    @shopify_session = ShopifyAPI::Session.new(domain: Constants::DOMAIN, token: Constants::ACCESS_TOKEN, api_version: @api_version, extra: nil)
+    @limit = Constants::LIMIT_PER_PAGE
+  end
+
+  def test_using_cursor_based_paginator(model)
+    ShopifyAPI::Base.activate_session(@shopify_session)
+
+    page = 1
+    self.class.benchmark_time(final_message_time(model)) do
+      records = self.class.benchmark_time(message(model, page)) do
+        model.find(:all, params: { limit: @limit })
+      end
+      while records.next_page?
+        page += 1
+        records = self.class.benchmark_time(message(model, page)) do
+          records.fetch_next_page
+        end
+      end
+    end
+  end
+
+  private
+
+  def message(model, page)
+    "Time to get page #{page} for #{model}s USING CURSOR BASED PAGINATION: "
+  end
+
+  def final_message_time(model)
+    "\e[32m Time to iterate over all #{model}s using CURSOR BASED PAGINATION: \e[0m"
+  end
+end
+
+paged_based_paginate = PageBasedPaginate.new
+paged_based_paginate.test_using_page(ShopifyAPI::Product)
+
+cursor_based_paginate = CurosrBasedPaginate.new
+cursor_based_paginate.test_using_cursor_based_paginator(ShopifyAPI::Product)
