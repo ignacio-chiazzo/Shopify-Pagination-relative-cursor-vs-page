c3d1afd2e82ad9d3fc86575f4b1db999c9eb8a4c
Ignacio Chiazzo <ignaciochiazzo@gmail.com>
2020-03-15 23:28:18 -0400

    Added colours to user input

--- cli_run.rb
+++ cli_run.rb
@@ -1,14 +1,15 @@
 require_relative 'src/test_pagination'
+require_relative 'src/utils/string'
 
 class CLI
   def self.run
     while true
       puts "To run the script in your shop you will need to enter the Shop Domain and the access Token.\n"
 
-      puts "Enter the shop DOMAIN: e.g. example.myshopify.com"
+      puts String.blue("Enter the shop DOMAIN: e.g. example.myshopify.com")
       domain = gets.chomp
 
-      puts "Enter yout access token"
+      puts String.blue("Enter your access token")
       access_token = gets.chomp
 
       test_pagination = ::TestPagination.new
